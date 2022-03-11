c4d978727e0007e24a95f0b3f3b3ff68565403df
Meri <mgogi13@freeuni.edu.ge>
2022-03-11 22:22:10 +0400

    Add validations to each form field in app.js file

--- js/app.js
+++ js/app.js
@@ -19,7 +19,28 @@ const registrationButton = document.querySelector('#registration-on-auth');
 const registrationButtonOnEmail = document.querySelector(
   '#registration-on-email'
 );
-const enterBtn = document.querySelector('#enter');
+
+let id = (id) => document.getElementById(id);
+let classes = (classes) => document.getElementsByClassName(classes);
+let email = id('email'),
+  enterBtn = id('enter'),
+  emailFld = id('email'),
+  passFld = id('pass'),
+  password = id('pass'),
+  firstName = id('first_name'),
+  lastName = id('last_name'),
+  region = id('region'),
+  school = id('school'),
+  grade = id('grade'),
+  recoveryEmail = id('recovery_email'),
+  recoveryPass = id('recovery_password'),
+  repeatRecPass = id('repeat_pass'),
+  registeringEmail = id('register_email'),
+  newPassword = id('new_password'),
+  repeatPassword = id('repeat_new_pass'),
+  successIcon = classes('success-icon'),
+  failureIcon = classes('failure-icon'),
+  errorMessage = classes('email-msg');
 
 const cards = [
   {
@@ -157,11 +178,19 @@ const removeClass = (element) => {
   element.classList.remove('active');
 };
 
-const errorMsg = (element) => {
+const errorMsgAdd = (element) => {
   element.classList.add('error');
 };
 
-const successMsg = (element) => {
+const errorMsgRemove = (element) => {
+  element.classList.remove('error');
+};
+
+const successMsgAdd = (element) => {
+  element.classList.add('success');
+};
+
+const successMsgRemove = (element) => {
   element.classList.remove('success');
 };
 
@@ -190,18 +219,51 @@ registrationBtnOnRecovery.addEventListener('click', () => {
   removeClass(passwordForm);
 });
 
-enterBtn.addEventListener('submit', (e) => {
+authForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  engine(emailFld, 0);
+  engine(passFld, 1);
+});
+
+registerForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  engine(firstName, 5);
+  engine(lastName, 6);
+  engine(region, 7);
+  engine(school, 8);
+  engine(grade, 9);
+  engine(registeringEmail, 10);
+  engine(newPassword, 11);
+  engine(repeatPassword, 12);
+});
+
+emailForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  engine(recoveryEmail, 4);
+});
+
+passwordForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  engine(recoveryPass, 2);
+  engine(repeatRecPass, 3);
 });
 
 let engine = (id, serial) => {
   if (id.value.trim() === '') {
-    id.style.border = '2px solid red';
-    failureIcon[serial].style.opacity = '1';
-    successIcon[serial].style.opacity = '0';
+    errorMsgAdd(id);
+    errorMsgAdd(failureIcon[serial]);
+    errorMsgAdd(errorMessage[serial]);
+    successMsgRemove(id);
+    successMsgRemove(successIcon[serial]);
   } else {
-    id.style.border = '2px solid green';
-    failureIcon[serial].style.opacity = '0';
-    successIcon[serial].style.opacity = '1';
+    errorMsgRemove(id);
+    errorMsgRemove(failureIcon[serial]);
+    errorMsgRemove(errorMessage[serial]);
+    successMsgAdd(id);
+    successMsgAdd(successIcon[serial]);
   }
 };
